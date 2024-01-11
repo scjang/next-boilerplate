f244a4188db825b3d538ca5aac2b1147735badb2
scjang <scjang.dev@gmail.com>
2024-01-11 18:34:07 +0900

    feat: Create `GET api`

---
+++ src/app/api/products/[id]/route.ts
@@ -0,0 +1,13 @@
+import { headers as nextHeaders } from 'next/headers'
+
+export async function GET(_request: Request, { params }: { params: { id: string } }) {
+  const headers = nextHeaders()
+
+  const { id } = params
+
+  return new Response(JSON.stringify({ id, name: 'Outer', price: 20000, like: 58 }), {
+    headers,
+    status: 200,
+    statusText: 'Success',
+  })
+}
