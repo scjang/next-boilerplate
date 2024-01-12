07c9de6d750efbe1f824e49a6026f9f7a50b57c5
scjang <scjang.dev@gmail.com>
2024-01-12 15:25:38 +0900

    refactor: Fix type inference at `peelOffAxios`

--- src/utils/peelOffAxios.ts
+++ src/utils/peelOffAxios.ts
@@ -2,14 +2,11 @@ import { AxiosError, AxiosResponse } from 'axios'
 
 import { ResponseError } from '~types/index'
 
-const peelOffAxios = async <TData>(fetcher: () => Promise<AxiosResponse<TData>>) => {
-  try {
-    const res = await fetcher()
-
-    return res
-  } catch (err) {
-    throw (err as AxiosError<ResponseError>).response?.data
-  }
-}
+const peelOffAxios = async <TData>(fetcher: () => Promise<AxiosResponse<TData>>) =>
+  fetcher()
+    .then((res) => res.data)
+    .catch((err: AxiosError<ResponseError>) => {
+      throw err.response?.data
+    })
 
 export default peelOffAxios
